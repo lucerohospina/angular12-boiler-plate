64d6d7000313ccfc1d4f311df1a36ac91edd06bc
Lucero Hospina <lucerojhp@gmail.com>
2023-03-17 08:12:26 -0500

    clean

--- src/app/services/questions.service.ts
+++ src/app/services/questions.service.ts
@@ -3,25 +3,15 @@ import { Observable, of } from 'rxjs';
 import { Sections } from '../models/section';
 import { Response } from '../models/response';
 import { Questions } from '../models/question';
-import { ApiService } from './api.service';
 import * as sections from '../mocks/section.json';
 import * as questions from '../mocks/question.json';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
-  // endpoint = '/sections.json';
-  // response: Response<Sections> = sections;
 
-  constructor(private apiService: ApiService) { }
-
-  // getSections(): Observable<any> {
-  //   return this.apiService.get<any>(
-  //     `${this.endpoint}`
-  //   );
-  // }
+  constructor() { }
 
   getSections(): Observable<Response<Sections>> {
     return of(sections);
